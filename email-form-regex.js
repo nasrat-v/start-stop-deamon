b349f0c7bdc1bed0776aebda2f478dec96bc0ed5
Valentin Nasraty <valentin.nasraty@epitech.eu>
2020-03-18 16:13:04 +0100

    Create email-form-regex.js

---
+++ email-form-regex.js
@@ -0,0 +1,15 @@
+$scope.emailAdressChange = function (subscriber) {
+  var emailAddressPattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+  if (subscriber == '') {
+    $scope.sendEmailShowError = true;
+		$('#sendByMailErrorPanel').html('<strong>Attention:</strong>&nbsp;Merci de bien vouloir renseigner une adresse email');
+	}
+	else if (!emailAddressPattern.test(subscriber)) {
+		$scope.sendEmailShowError = true;
+		$('#sendByMailErrorPanel').html('<strong>Attention:</strong>&nbsp;L\'adresse email semble incorrecte');
+	}
+	else {
+		$scope.sendEmailShowError = false;
+	}
+}
